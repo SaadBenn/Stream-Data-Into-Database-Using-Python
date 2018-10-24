6dbe6fed306cd52f35511f33f4ea8edc6446fd8e
Saad <saadbennmushtaq@gmail.com>
2018-10-24 19:26:42 -0400

    Get the raw data as JSON and store it into db

--- StreamListener.py
+++ StreamListener.py
@@ -1,4 +1,9 @@
 import tweepy
+import json
+from dateutil import parser
+from mysql.connector import Error
+
+from StreamDataIntodb import Connector
 
 
 class StreamListener(tweepy.StreamListener):
@@ -15,3 +20,28 @@ class StreamListener(tweepy.StreamListener):
         else:
             # disconnect the stream
             return False
+
+    def on_data(self, raw_data):
+
+        try:
+            raw_data = json.loads(raw_data)
+
+            if 'text' in raw_data:
+                username = raw_data['user']['screen_name']
+                created_at = parser.parse(raw_data['created_at'])
+                tweet = raw_data['text']
+                retweet_count = raw_data['retweet_count']
+
+                if raw_data['place'] is not None:
+                    place = raw_data['place']['country']
+                    print(place)
+                else:
+                    place = None
+
+                location = raw_data['user']['location']
+
+                Connector.connect(username, created_at, tweet, retweet_count, place, location)
+                print("Tweet collected at: {} ".format(str(created_at)))
+
+        except Error as err:
+            print(err)
